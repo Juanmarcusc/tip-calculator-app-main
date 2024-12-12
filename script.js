361006e13ebdbcdb8903bcf83a385d2c8a13834f
Juan Marcus <eng.juanmarcus@gmai.com>
2024-12-12 11:59:54 -0300

    Js finish

--- script.js
+++ script.js
@@ -2,16 +2,35 @@ let valorBill = "";
 function inputBill() {
     const valor = document.getElementById('inputBill').value
     valorBill = parseFloat(valor);
+    console.log("Valor da conta:", valorBill)
     
 }
 let valorDesconto = "";
 function pegarDesconto(desconto) {
     valorDesconto = parseFloat(desconto)
+    console.log("Valor do desconto:", valorDesconto);
     
 } 
 
+let valorPeople = ""; 
+function inputPeople() {
+    const valorDePessoas= document.getElementById('inputPeople').value
+    valorPeople = parseFloat(valorDePessoas)
+}
+
 function valorTotalConta() {
-    const resultado = valorBill * valorDesconto;
+    const resultado = valorBill - (valorBill * valorDesconto);
     console.log(resultado)
 
+    const mostrarResultadoBill = document.getElementById('contaBillComDesconto')
+    mostrarResultadoBill.textContent = `$ ${resultado.toFixed(2)}`;
+
+}
+
+function valorTotalPorPessoa() {
+    const resultado =  [valorBill - (valorBill * valorDesconto)]/ valorPeople;
+    console.log(resultado)
+
+    const mostrarResultadoParaCadaPessoa = document.getElementById('contaParaCadaPessoa')
+    mostrarResultadoParaCadaPessoa.textContent = `$ ${resultado.toFixed(2)}`;
 }
